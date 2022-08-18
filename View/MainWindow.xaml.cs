857077a8f797ff6beeaa00fc9b69578263a4f1e4
markls <isxakov.mark@gmail.com>
2022-08-18 14:20:27 +0500

    Начальный коммит. Реализован базовый функционал.

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CrossesCircles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BitmapImage circle = new BitmapImage(new Uri("../Resources/Circle.png", UriKind.Relative));
+        BitmapImage cross = new BitmapImage(new Uri("../Resources/Cross.png", UriKind.Relative));
+        BitmapImage source;
+        Grid grid;
+        public MainWindow()
+        {
+            InitializeComponent();
+            grid = FindName("MainGrid") as Grid;
+            source = cross;
+        }
+
+        #region Button Clicked and Initializing image
+        private void BtnClicked(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            button.Visibility = Visibility.Collapsed;
+            int column = Grid.GetColumn(button);
+            int row = Grid.GetRow(button);
+            Image image = FindImage(row, column);
+            image.Source = source;
+            source = source == circle ? cross : circle;
+        }
+        /// <summary>
+        /// Find the location of Image by row and column of the Grid.
+        /// </summary>
+        /// <param name="row">Row of a button in the Grid</param>
+        /// <param name="column">Column of a button in the Grid</param>
+        /// <returns></returns>
+        Image FindImage(int row, int column)
+        {
+            foreach(var c in grid.Children.OfType<Image>())
+            {
+                if (Grid.GetRow(c) == row && Grid.GetColumn(c) == column)
+                    return c;
+            }
+            return null;
+        }
+        #endregion
+        //TODO: масштабирование окна
+    }
+}
